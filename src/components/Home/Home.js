ffc93ecd63229375fea2794bdebbdbe2c3c88598
Umit Turk <uyt.turk@gmail.com>
2021-06-08 01:37:13 +0300

    grab movie from db

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ImgSlider from '../ImgSlider/ImgSlider'
 import Movies from '../Movies/Movies'
 import Viewers from '../Viewers/Viewers'
+import db from '../../firebase';
+import { useDispatch } from "react-redux";
+import { setMovies } from "../../features/movie/movieSlice"
 
 import { Container } from './homeStyle'
 
 
 function Home() {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        db.collection("movies").onSnapshot((snapshot) => {
+            let tempMovies = snapshot.docs.map((doc) => {
+                return {id: doc.id, ...doc.data()}
+            })
+            dispatch(setMovies(tempMovies));
+        })
+    }, [])
+
+
     return (
         <Container>
             <ImgSlider />
--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,35 +1,25 @@
 import React from 'react'
 import { Container, Content, Wrap } from './moviesStyle'
+import {useSelector} from "react-redux"
+import { selectMovies } from '../../features/movie/movieSlice';
 
 function Movies() {
+
+    const movies = useSelector(selectMovies);
+    console.log(movies);
+
     return (
         <Container>
             <h4>Recomended for You</h4>
             <Content>
-                <Wrap>
-                    <img src="https://scontent.fist2-4.fna.fbcdn.net/v/t1.6435-9/89462424_2638544703088801_7918040516782981120_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=OUIYDOu5d7AAX9k7Hzc&_nc_ht=scontent.fist2-4.fna&oh=6504b83653e042772d49f4897056adc3&oe=60E25170"/>
-                </Wrap>
-                <Wrap>
-                    <img src="https://scontent.fist2-4.fna.fbcdn.net/v/t1.6435-9/89462424_2638544703088801_7918040516782981120_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=OUIYDOu5d7AAX9k7Hzc&_nc_ht=scontent.fist2-4.fna&oh=6504b83653e042772d49f4897056adc3&oe=60E25170"/>
-                </Wrap>
-                <Wrap>
-                    <img src="https://scontent.fist2-4.fna.fbcdn.net/v/t1.6435-9/89462424_2638544703088801_7918040516782981120_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=OUIYDOu5d7AAX9k7Hzc&_nc_ht=scontent.fist2-4.fna&oh=6504b83653e042772d49f4897056adc3&oe=60E25170"/>
-                </Wrap>
-                <Wrap>
-                    <img src="https://scontent.fist2-4.fna.fbcdn.net/v/t1.6435-9/89462424_2638544703088801_7918040516782981120_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=OUIYDOu5d7AAX9k7Hzc&_nc_ht=scontent.fist2-4.fna&oh=6504b83653e042772d49f4897056adc3&oe=60E25170"/>
-                </Wrap>
-                <Wrap>
-                    <img src="https://scontent.fist2-4.fna.fbcdn.net/v/t1.6435-9/89462424_2638544703088801_7918040516782981120_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=OUIYDOu5d7AAX9k7Hzc&_nc_ht=scontent.fist2-4.fna&oh=6504b83653e042772d49f4897056adc3&oe=60E25170"/>
-                </Wrap>
-                <Wrap>
-                    <img src="https://scontent.fist2-4.fna.fbcdn.net/v/t1.6435-9/89462424_2638544703088801_7918040516782981120_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=OUIYDOu5d7AAX9k7Hzc&_nc_ht=scontent.fist2-4.fna&oh=6504b83653e042772d49f4897056adc3&oe=60E25170"/>
-                </Wrap>
-                <Wrap>
-                    <img src="https://scontent.fist2-4.fna.fbcdn.net/v/t1.6435-9/89462424_2638544703088801_7918040516782981120_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=OUIYDOu5d7AAX9k7Hzc&_nc_ht=scontent.fist2-4.fna&oh=6504b83653e042772d49f4897056adc3&oe=60E25170"/>
-                </Wrap>
-                <Wrap>
-                    <img src="https://scontent.fist2-4.fna.fbcdn.net/v/t1.6435-9/89462424_2638544703088801_7918040516782981120_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=OUIYDOu5d7AAX9k7Hzc&_nc_ht=scontent.fist2-4.fna&oh=6504b83653e042772d49f4897056adc3&oe=60E25170"/>
-                </Wrap>
+                {movies && 
+                    movies.map((movie) => (
+                        <Wrap key={movie.id}>
+                            <img src={movie.cardImg}/>
+                        </Wrap>
+                    ))
+                }
+              
             </Content>
         </Container>
     )
